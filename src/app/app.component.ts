064fd5ab839ce8c530bd47d7fcbbe048d299ba77
pipe <felipe.parra@blackmind.cl>
2023-05-12 21:35:40 -0400

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<app-navbar></app-navbar><router-outlet></router-outlet><app-footer></app-footer>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
